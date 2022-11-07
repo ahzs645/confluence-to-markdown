1018a89fc63d958112c41369621f7b4ef1ce3aba
Espen Henriksen <espen@oslomarketsolutions.no>
2022-11-07 23:42:20 +0100

    Handle images

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const fsPromises = require('fs/promises');
+const path = require('path');
 const { JSDOM } = require('jsdom');
 const prettier = require('prettier');
 
@@ -14,7 +16,7 @@ if (process.argv.length !== 4) {
 const [inputdir, outputdir] = process.argv.slice(2);
 
 const getFilepath = (node) =>
-  [...(node.querySelector('#breadcrumbs')?.children || [])]?.map((node) =>
+  [...(node.ownerDocument.querySelector('#breadcrumbs')?.children || [])]?.map((node) =>
     node.textContent.trim(),
   );
 
@@ -28,6 +30,27 @@ const denylist = ['page-metadata', 'footer', 'breadcrumb-section'];
 const shouldBeDropped = (node) =>
   denylist.includes(node.className) || denylist.includes(node.id);
 
+const imageExists = filepath => {
+  const parsedImgPath = path.resolve(inputdir);
+  const [ file ] = filepath.split(parsedImgPath + path.sep).slice(1);
+  return fs.existsSync([inputdir, file].join(path.sep));
+}
+
+const copycommands = [];
+const extractImage = node => {
+  const parsedImgPath = path.resolve(inputdir);
+  const [ file ] = node.src.split(parsedImgPath + path.sep).slice(1);
+  const { base } = path.parse(file);
+  const filename = base.replace(/\?.*/, '');
+  const filepath = getFilepath(node);
+  const outdir = [outputdir, ...filepath].join(path.sep);
+  const absoluteFromFile = path.resolve([inputdir, file].join(path.sep)).replace(/\?.*/, '');
+  copycommands.push([absoluteFromFile, [outdir, 'images', filename].join(path.sep)]);
+  console.log(`Found image ${file}, copying to ${outdir}`)
+
+  return filename;
+}
+
 const htmlFileIntoMd = async (mainNode) => {
   const toMarkdownChildren = (nodes) =>
     [...nodes.childNodes].flatMap(toMarkdown).join('');
@@ -36,12 +59,19 @@ const htmlFileIntoMd = async (mainNode) => {
     switch (node.nodeName) {
       case '#text':
         //if (isOnlyWhitespace(str)) return [];
-        const str = node.textContent;
+        const str = node.textContent
+          .replaceAll('<', '&lt;')
+          .replaceAll('>', '&gt;');
         return [
           startsWithNewline(str) ? '\n' : '',
           str.trim(),
           endsWithNewline(str) ? '\n' : '',
         ];
+      case 'IMG':
+        if (!node.src) return [];
+        if (!imageExists(node.src)) return [];
+        const filename = extractImage(node);
+        return [' ![', toMarkdownChildren(node), '](images/', filename, ') '];
       case 'BR':
         return ['\n'];
       case 'P':
@@ -101,6 +131,8 @@ const htmlFileIntoMd = async (mainNode) => {
       case 'TH':
       case 'TD':
         return toMarkdownChildren(node).trim();
+      case 'SCRIPT':
+        return [];
       default:
         return toMarkdownChildren(node);
     }
@@ -116,24 +148,36 @@ const main = async () => {
   try {
     const files = await fsPromises.readdir(inputdir, { withFileTypes: true });
 
-    const promises = files
+    const convertPromises = files
       .filter((file) => file.name.endsWith('.html') && !file.isDirectory())
       .map(async (file) => {
-        const { window } = await JSDOM.fromFile(file.name);
-        const mainNode = window.document.querySelector('#main');
+        const { window } = await JSDOM.fromFile([inputdir, file.name].join(path.sep));
+        const mainNode = window.document.querySelector('#content');
 
         const md = await htmlFileIntoMd(mainNode);
         if (!md) return;
 
         const filename = window.document.title.replaceAll('/', '_') + '.md';
         const filepath = getFilepath(mainNode);
-        const outputdirPath = ['.', outputdir, ...filepath].join('/');
+        const outputdirPath = ['.', outputdir, ...filepath].join(path.sep);
 
         await fsPromises.mkdir(outputdirPath, { recursive: true });
-        return fsPromises.writeFile(outputdirPath + '/' + filename, md);
+        return fsPromises.writeFile(outputdirPath + path.sep + filename, md);
       });
 
-    await Promise.all(promises);
+    await Promise.all(convertPromises);
+
+    const copyPromises = copycommands
+      .map(async ([from, to]) => {
+        try {
+          const { dir } = path.parse(to);
+          await fsPromises.mkdir(dir, { recursive: true });
+          await fsPromises.copyFile(from, to);
+        } catch (err) {
+          console.error('Failed to copy file', err)
+        }
+      });
+    await Promise.all(copyPromises);
   } catch (err) {
     console.error(err);
     process.exit(1);
